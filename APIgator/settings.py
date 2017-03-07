ee3e2879c89517928b83d0ee6142eeaf4a652487
EndermanAPM <xavier1997@gmail.com>
2017-03-08 00:35:40 +0100

    Elongates sleep to check if fail is the containers fault

--- APIgator/settings.py
+++ APIgator/settings.py
@@ -3,10 +3,10 @@ import os
 MONGO_PORT = 27017
 
 
-# try:
-MONGO_HOST = os.environ['MONGO_PORT_27017_TCP_ADDR']
-# except KeyError:
-#     MONGO_HOST = "localhost"
+try:
+    MONGO_HOST = os.environ['MONGO_PORT_27017_TCP_ADDR']
+except KeyError:
+    MONGO_HOST = "localhost"
 
 DOMAIN = {
     'lists': {}
