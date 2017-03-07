2c4f4ae25a52e9453c07bcb3211558476eefddfc
EndermanAPM <xavier1997@gmail.com>
2017-03-08 00:06:11 +0100

    If Mongo environ key is not founds connects to localhost db

--- APIgator/settings.py
+++ APIgator/settings.py
@@ -2,11 +2,11 @@ import os, requests
 
 MONGO_PORT = 27017
 
-if requests.get("http://localhost:"+str(MONGO_PORT)).status_code==200:
-    MONGO_HOST = "localhost"
-else:
-    MONGO_HOST = os.environ['MONGO_PORT_27017_TCP_ADDR']
 
+try:
+    MONGO_HOST = os.environ['MONGO_PORT_27017_TCP_ADDR']
+except KeyError:
+    MONGO_HOST = "localhost"
 
 DOMAIN = {
     'lists': {}
