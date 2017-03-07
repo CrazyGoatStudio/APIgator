d4b41ed9ed89d0431b52b3f066ec62873e6b8a0b
EndermanAPM <xavier1997@gmail.com>
2017-03-08 00:01:15 +0100

    Adds build badage, reduces yml sleep and connects to localhost mongodb database before getting docker mongodb ip

--- APIgator/settings.py
+++ APIgator/settings.py
@@ -1,6 +1,12 @@
-import os
-MONGO_HOST = os.environ['MONGO_PORT_27017_TCP_ADDR']
+import os, requests
+
 MONGO_PORT = 27017
+
+if requests.get("http://localhost:"+str(MONGO_PORT)).status_code==200:
+    MONGO_HOST = "localhost"
+else:
+    MONGO_HOST = os.environ['MONGO_PORT_27017_TCP_ADDR']
+
 
 DOMAIN = {
     'lists': {}
